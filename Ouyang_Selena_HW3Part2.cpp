46884744e0a6bbc3104599d44f8cda51e58f603d
Selenao10 <selenao10@nycstudents.net>
2024-10-20 12:41:56 -0400

    Update Ouyang_Selena_HW3Part2.cpp

--- Ouyang_Selena_HW3Part2.cpp
+++ Ouyang_Selena_HW3Part2.cpp
@@ -15,12 +15,12 @@ int main() {
 
   //assigning variables
   int numWithdrawals = 0, numDeposits = 0, numTransactions = 0; 
-  float totalMoney, transactionAmount;  
+  float totalMoney, transactionAmount, depositAmount = 0, withdrawalAmount = 0;  
   char typeofTransaction; 
 
   // open files
-  inFile.open("Ouyang_Selena_outData.txt");
-  outFile.open("outData.txt");
+  inFile.open("transaction.txt");
+  outFile.open("Ouyang_Selena_outData.txt");
 
   //two decimal places
   outFile << fixed << setprecision(2); 
@@ -29,34 +29,64 @@ int main() {
   inFile >> totalMoney;
   outFile << "Starting Balance: " << '$' << totalMoney << endl << endl; //prints starting balance
   outFile << setfill('_') << setw(4) << "Type" << setw(13) << "Amount" << setw(23) << "Balance" << endl;
-  
+
   //while loop to read in file
   while (!inFile.eof()) {
-    
+
     inFile >> typeofTransaction >> transactionAmount; //reads transaction type and amount
-    
+
     numTransactions++; //increments number of transactions by one
 
     //if the transactions is a deposit
     if (typeofTransaction == 'D') {
       numDeposits++; //number of deposits increments by one
       totalMoney += transactionAmount; //adds transaction amount to starting amount
+      depositAmount += transactionAmount; //updates total deposit amount
       outFile << setw (1) << typeofTransaction << setw(16) << transactionAmount << setw(23) << totalMoney << endl; //prints type, amount, and remaining balance 
     }
     //else if the transactions is a withdrawal
     else if (typeofTransaction == 'W') {
       numWithdrawals++; //number of withdrawals increments by one
       totalMoney -= transactionAmount; //subtracts transaction amount from starting amount
+      withdrawalAmount += transactionAmount; //updates total withdrawal amount
       outFile << setw (1) << typeofTransaction << setw(16) << transactionAmount << setw(23) << totalMoney << endl; //prints type, amount, and remaining balance 
     }
   }
-  
-  //prints final balance, total withdrawals, total deposits, and total transactions
-  outFile << endl << "Final Balance: $" << totalMoney << endl << "Total Withdrawals: " << numWithdrawals << endl << "Total Deposits: " << numDeposits << endl << "Total Transacitons: " << numTransactions;
-  
+
+  //prints information about final balance, withdrawals, deposits, transactions
+  outFile << endl << "Final Balance: $" << totalMoney << endl << endl;
+  outFile << "Number of Withdrawals: " << numWithdrawals << endl;
+  outFile << "Total Withdrawl Amount: $" << withdrawalAmount << endl << endl;
+  outFile << "Number of Deposits: " << numDeposits << endl;
+  outFile << "Total Deposit Amount: $" << depositAmount << endl << endl;
+  outFile << "Number of Total Transactions: " << numTransactions;
+
   // close files
   inFile.close();
   outFile.close();
 
   return 0;
 }
+
+/*
+Starting Balance: $5000.00
+
+Type_______Amount________________Balance
+W__________200.00________________4800.00
+W__________800.00________________4000.00
+W___________40.00________________3960.00
+D___________90.00________________4050.00
+D__________100.00________________4150.00
+D_________1000.00________________5150.00
+W___________20.00________________5130.00
+
+Final Balance: $5130.00
+
+Number of Withdrawals: 4
+Total Withdrawl Amount: $1060.00
+
+Number of Deposits: 3
+Total Deposit Amount: $1190.00
+
+Number of Total Transactions: 7
+*/
